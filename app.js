c5edc5348ff73909742ad354204041312c9a6b88
Penny910443 <g9523519@gmail.com>
2023-05-17 10:16:52 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
 const express = require('express')
-const app = express()
-const port = 3000
-const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 
-const exphbs = require('express-handlebars')
-const Restaurant = require('./models/restaurant')
 const routes = require('./routes')
+require('./config/mongoose')
 
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+const app = express()
+const port = 3000
 
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -33,9 +16,6 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
-
-
-
 
 app.listen(port, () => {
   console.log(`Express is listening localhost:${port}`)
