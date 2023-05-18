8e4eb3c6d28770575f51719b941a2888283ff794
Penny910443 <g9523519@gmail.com>
2023-05-18 10:36:50 +0800

    feature: update dropdown function.

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,8 +4,25 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
+  const sort = req.query.sort
+  const sortMethod = {}
+  switch (sort) {
+    case 'asc':
+      sortMethod.name_en = 'asc';
+      break;
+    case 'desc':
+      sortMethod.name_en = 'desc';
+      break;
+    case 'category':
+      sortMethod.category = 'asc';
+      break;
+    case 'location':
+      sortMethod.location = 'asc';
+      break;
+  }
   Restaurant.find()
     .lean()
+    .sort(sortMethod)
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.error(error))
 })
